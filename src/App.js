5330ec00859aff707ba117f4ca371ed4eea03004
KeaweAquarian <keaweaquarian@gmail.com>
2023-02-13 16:32:40 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask";
 import { useState } from "react"
 
 function App() {
@@ -25,10 +26,31 @@ function App() {
     }
 ])
 
+//Add task
+const addTask = (task) => {
+  console.log(task)
+}
+
+//Delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+//Toggle reminder
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => task.id === id
+  ? { ...task, reminder: !task.reminder}  : task
+  ))
+}
+
   return (
     <div className="container">
   <Header />
-  <Tasks tasks={tasks}/>
+  <AddTask onAdd={addTask} />
+  {tasks.length > 0 ? <Tasks tasks={tasks} 
+  onDelete= {deleteTask} onToggle={toggleReminder}
+  /> : "No tasks"}
     </div>
   );
 }
