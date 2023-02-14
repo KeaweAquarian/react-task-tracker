ea1eed53df183b1a1c5b4c7fd7ac0e29cfdd5194
KeaweAquarian <keaweaquarian@gmail.com>
2023-02-13 22:14:00 -0500

    updates

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+    return (
+       <footer>
+        <p>Copytight &copy; 2023</p>
+        <Link to="/about">About</Link>
+       </footer>
+    )
+    }
+
+export default Footer
