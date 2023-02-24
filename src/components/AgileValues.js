47383bdcf2377d696e119b1a6de3085ba4bc71f9
KeaweAquarian <keaweaquarian@gmail.com>
2023-02-24 15:33:09 -0500

    added background color

---
+++ src/components/AgileValues.js
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+import image from '../Images/agileFour.png';
+
+
+
+const AgileValues = () => {
+    return (
+      <div>
+        <img className = "agileImg" src={image} alt="" />
+        <Link to="/">Go Back</Link>
+      </div>
+    )
+  }
+
+  export default AgileValues
+
