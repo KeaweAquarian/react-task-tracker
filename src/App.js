b0841e392cced9ae89dedcb49c8e065c9b8cddbb
Keawe Aquarian <111657049+KeaweAquarian@users.noreply.github.com>
2023-03-03 21:33:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -75,11 +75,9 @@ const App = () => {
     const res = await fetch(`https://json-server-xi-seven.vercel.app/tasks/${id}`, {
       method: 'DELETE',
     })
-setTasks(tasks.filter((task) => task.id !== id))
-  //Set responce type.
-    res.status === 200
-      ? setTasks(tasks.filter((task) => task.id !== id))
-      : alert('Error task already deleted, refresh page!')
+  
+    
+    
   }
 
     //Toggle Tested Task
